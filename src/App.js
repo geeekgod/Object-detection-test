97391b2679e28cc1cd48239c331856d47b4b4ce8
Rishabh Singh <ris8645208@gmail.com>
2021-12-08 20:27:58 +0530

    feat: first test for object detection

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { useRef, useEffect, memo } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawRect } from "./utilities";
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    setInterval(() => {
+      detect(net);
+    }, 20);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+      const obj = await net.detect(video);
+      console.log(obj);
+
+      const ctx = canvasRef.current.getContext("2d");
+
+      drawRect(obj, ctx);
+    }
+  };
+
+  useEffect(() => {
+    runCoco();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            maxWidth: 640,
+            maxHeight: 480,
+            width: "100%",
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            maxWidth: 640,
+            maxHeight: 480,
+            width: "100%",
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default memo(App);
